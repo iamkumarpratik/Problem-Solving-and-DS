2a0f0214770e443f3d0f8fd75bfb3bc5432c018e
Kumar Pratik <>
2022-06-21 23:46:26 +0530

    time and space complexity.

---
+++ time_and_space_complexity.py
@@ -0,0 +1,14 @@
+# Time complexity.
+
+# Time taken to run a particular algorithm. Depending on the time complexity;
+# it is assesed how efficient or fast an algorithm is to solve a problem.
+
+# Notation: Big O.
+
+# Ex: O(1), O(N), O(N^2), and O(log n).
+
+# Time complexity: O(N^2) > O(N) > O(log n)> O(1).
+
+# Space Complexity.
+
+# Total space taken by the algorithm data with comparasion to the input size.
